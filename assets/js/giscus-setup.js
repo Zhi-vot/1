4b61c99ff39eb047b74456623c3290df55dde756
Zhi-vot <926293008@qq.com>
2025-12-24 11:31:58 +0000

    Deploying to gh-pages from @ Zhi-vot/1@0e5d41e3dff4eab313749beaa673335e0396b4e9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "Zhi-vot/1",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
